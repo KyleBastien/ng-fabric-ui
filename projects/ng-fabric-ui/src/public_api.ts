2d8c0ca3597c2bb09ec8e5f528ac69d487ca2edd
Kyle McNutt <kymcnutt@microsoft.com>
2019-01-22 11:56:47 -0800

    Changing library to use latest Angular version.
    This moves the Angular version to v7.x.
    Also moved to Angular CLI supported library format with "projects" style repo.

---
+++ projects/ng-fabric-ui/src/public_api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of ng-fabric-ui
+ */
+
+export * from './lib/label/label.module';
+export * from './lib/label/label.component';
+
+export * from './lib/spinner/spinner.module';
+export * from './lib/spinner/spinner.component';
--- public_api.ts
+++
@@ -1,1 +0,0 @@
-export * from './src/app/lib/label/label.module';
